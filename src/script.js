0ecd8be2b8dbb45f96d525e1cc4582a226e5913f
Javier Sanahuja <bannss1@gmail.com>
2020-05-31 05:09:09 +0200

    Script using VideoCall

--- src/script.js
+++ src/script.js
@@ -11,87 +11,122 @@ const client = new VideoCall({
         }
     ],
     iceCandidatePoolSize: 40,
-    onLocalStream: function(stream){
-        document.querySelector('video#local').srcObject = stream;
-    },
-    onRemoteStreamStart: function(callId, stream){
-        var video = document.createElement("video");
-        video.autoplay = "true";
-        video.srcObject = stream;
-        video.id = "remote" + callId;
-        document.querySelector('#videos').appendChild(video);
-    },
-    onRemoteStreamClose: function(callId){
-        var id = "remote" + callId;
-        var video = document.querySelector("#" + id);
-        document.querySelector('#videos').removeChild(video);
-    },
-    onUpdateStreamOptions: function(streamOptions){
-        if(streamOptions.video){
-            document.querySelector("#toggleCam").innerText = "Disable Cam";
-        }else{
-            document.querySelector("#toggleCam").innerText = "Enable Cam";
-        }
+    // onRemoteStreamStart: function(callId, stream){
+    //     var video = document.createElement("video");
+    //     video.autoplay = "true";
+    //     video.srcObject = stream;
+    //     video.id = "remote" + callId;
+    //     document.querySelector('#videos').appendChild(video);
+    // },
+    // onRemoteStreamClose: function(callId){
+    //     var id = "remote" + callId;
+    //     var video = document.querySelector("#" + id);
+    //     document.querySelector('#videos').removeChild(video);
+    // },
 
-        if(streamOptions.microphone){
-            document.querySelector("#toggleMicrophone").innerText = "Disable Microphone";
-        }else{
-            document.querySelector("#toggleMicrophone").innerText = "Enable Microphone";
-        }
+    eventsHandler: function(e){
+        switch(e.type){
+            // Errors
+            case "error_message_noroom":
+                break;
+            case "error_create_alreadyinaroom":
+                break;
+            case "error_join_alreadyinaroom":
+                break;
+            case "error_leave_noroom":
+                break;
+            case "error_kick_notallowed":
+                break;
+            case "error_ban_notallowed":
+                break;
+            case "error_unban_notallowed":
+                break;
+            case "error_media_nopermission":
+                break;
+            case "error_webrtc_nosupport":
+                alert("Your browser does not support WebRTC");
+                break;
+            case "error_toggle_nocamera":
+                break;
+            case "error_toggle_nomicrophone":
+                break;
 
-        if(streamOptions.audio){
-            document.querySelector("#toggleAudio").innerText = "Disable Sound";
-        }else{
-            document.querySelector("#toggleAudio").innerText = "Enable Sound";
-        }
-    },
-    onPermissionDenied: function(error){
-        alert("Permission denied. Error:", error);
-    },
-    onUnsupportedWebRTC: function(){
-        alert("Your browser does not support WebRTC");
-    },
-    onUserEvent: function(event){
-        switch(event.type){
-            case "created":
+            // Local
+            case "local_ready":
+                console.log("Connected to signaling server");
+                break;
+            case "local_media":
+                document.querySelector('video#local').srcObject = e.stream;
+                break;
+            case "local_created":
                 var url = "https://webrtc.sowecms.com/client/autojoin.html#" + event.room.id + ":" + document.querySelector('#createRoomPassword').value;
                 document.querySelector("#roomid").innerHTML = "<a href='" + url +"'>" + url + "</a>";
                 break;
-            case "joined":
+            case "local_joined":
                 break;
-            case "left":
+            case "local_left":
                 break;
-            case "kicked":
+            case "local_kicked":
                 break;
-            case "banned":
+            case "local_banned":
                 break;
-            case "unbanned":
+            case "local_unbanned":
+                break;
+            case "local_toggle":
+                switch(e.resource){
+                    case "camera":
+                        document.querySelector("#toggleCam").innerText =
+                            (e.status) ? "Disable Cam" : "Enable Cam";
+                        break;
+                    case "microhpone":
+                        document.querySelector("#toggleMicrophone").innerText =
+                            (e.status) ? "Disable Microphone" : "Enable Microphone";
+                        break;
+                    case "audio":
+                        e.status = !document.querySelector('video#remote').muted;
+                        document.querySelector('video#remote').muted = e.status 
+                        document.querySelector("#toggleAudio").innerText =
+                            (e.status) ? "Disable Sound" : "Enable Sound";
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            // Remote
+            case "remote_message":
+                var p = document.createElement("p");
+                p.innerHTML = "<strong>" + e.user.name + "</strong>: " + sanitize(e.message);
+                document.querySelector("#chat-messages").appendChild(p);
+                break;
+            case "remote_joined":
+                break;
+            case "remote_left":
+                break;
+            case "remote_kicked":
+                break;
+            case "remote_banned":
+                break;
+            case "remote_unbanned":
+                break;
+            case "remote_toggle":
                 break;
         }
     },
-    onRoomEvent: function(event){
-        switch(event.type){
-            case "resource":
-                break;
-            case "message":
-                var p = document.createElement("p");
-                if(peer.socket.id == event.client.id)
-                    p.innerText = "You: " + event.data;
-                else
-                    p.innerText = "Other: " + event.data;
-                document.querySelector("#chat-messages").appendChild(p);
-                    
-                break;
-            case "joined":
-                break;
-            case "left":
-                break;
-            case "kicked":
-                break;
-            case "banned":
-                break;
-            case "unbanned":
-                break;
-        }
-    }
 });
+
+// https://stackoverflow.com/a/48226843
+function sanitize(string) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        "/": '&#x2F;',
+    };
+    const reg = /[&<>"'/]/ig;
+    return string.replace(reg, function(){
+        return map[match];
+    });
+}
