201a1eab8dcd638d85a60914c9f67277e8d71d3a
Javier Sanahuja <bannss1@gmail.com>
2020-05-31 05:08:35 +0200

    VideoCall events and toggles

--- src/VideoCall.js
+++ src/VideoCall.js
@@ -8,12 +8,16 @@
     }
 }(this, function() {
     return function(config) {
+        this.id = null;
+        this.room = null;
         this.config = config;
         this.videoStream = null;
         this.audioStream = null;
-        this.id = null;
-        this.room = null;
         var self = this;
+
+        if(!webrtcsupport.support || !webrtcsupport.mediaStream){
+            self.fireEvent("error_webrtc_nosupport");
+        }
 
         // Helpers
         this.hasRoom = function(){
@@ -25,14 +29,46 @@
         }
 
         this.fireEvent = function(type, data){
-            if(typeof this.config.eventHandler !== "undefined"){
-                this.config.eventHandler(Object.assign(
+            if(typeof this.config.eventsHandler !== "undefined"){
+                this.config.eventsHandler(Object.assign(
                     (data || {}), {type: type}
                 ));
             }
         }
         
-        // Room actions
+        // actions
+        this.toggleCamera = function(){
+            if(typeof this.videoStream.getVideoTracks !== "undefined"){
+                var videoTracks = this.videoStream.getVideoTracks();
+                if(typeof videoTracks[0] !== "undefined"){
+                    videoTracks[0].enabled = !videoTracks[0].enabled;
+                    this.socket.emit("toggle", {resource: "camera", status: videoTracks[0].enabled});
+                    this.fireEvent("toggle_camera", {status: videoTracks[0].enabled});
+                    return true;
+                }
+            }
+            this.fireEvent("error_toggle_nocamera");
+            return false;
+        };
+
+        this.toggleMicrophone = function(){
+            if(typeof this.audioStream.getAudioTracks !== "undefined"){
+                var audioTracks = this.audioStream.getAudioTracks();
+                if(typeof audioTracks[0] !== "undefined"){
+                    audioTracks[0].enabled = !audioTracks[0].enabled;
+                    this.socket.emit("toggle", {resource: "microphone", status: audioTracks[0].enabled});
+                    this.fireEvent("toggle_microphone", {status: audioTracks[0].enabled});
+                    return true;
+                }
+            }
+            this.fireEvent("error_toggle_nomicrophone");
+            return false;
+        };
+
+        this.toggleAudio = function(){
+            this.fireEvent("toggle_microphone", {status: audioTracks[0].enabled});
+        };
+
         this.messageRoom = function(message){
             if(!this.hasRoom()){
                 this.fireEvent("error_message_noroom");
@@ -67,7 +103,7 @@
 
         this.kickFromRoom = function(clientId){
             if(!this.isOwner){
-                this.fireEvent("error_kick_nopermission");
+                this.fireEvent("error_kick_notallowed");
                 return false;
             }
             this.socket.emit("kick", clientId);
@@ -75,7 +111,7 @@
 
         this.banFromRoom = function(clientId){
             if(!this.isOwner){
-                this.fireEvent("error_ban_nopermission");
+                this.fireEvent("error_ban_notallowed");
                 return false;
             }
             this.socket.emit("ban", clientId);
@@ -83,7 +119,7 @@
 
         this.unbanFromRoom = function(clientId){
             if(!this.isOwner){
-                this.fireEvent("error_ban_nopermission");
+                this.fireEvent("error_unban_notallowed");
                 return false;
             }
             this.socket.emit("unban", clientId);
@@ -102,9 +138,7 @@
                 
                 this.fireEvent("local_media", {stream: this.videoStream});
             }catch(e){
-                if(typeof this.config.onPermissionDenied !== "undefined"){
-                    this.config.onPermissionDenied(e);
-                }
+                this.fireEvent("error_media_nopermission", {error: e});
             }
         };
 
@@ -112,50 +146,47 @@
             // Binding socket
             this.socket = io(config.server);
             this.socket.on('connect', function () {
-                console.log("Connected IO:", self.socket.id);
+                self.fireEvent("local_ready");
             });
 
             // Local client events
             this.socket.on('created', function(room){
-                self.buildClientEvent("created", room);
+                self.fireEvent("local_created", {room: room});
             });
             this.socket.on('joined', function(room){
-                self.buildClientEvent("joined", room);
+                self.fireEvent("local_joined", {room: room});
             });
             this.socket.on('left', function(room){
-                self.buildClientEvent("left", room);
+                self.fireEvent("local_left", {room: room});
             });
             this.socket.on('kicked', function(room){
-                self.buildClientEvent("kicked", room);
+                self.fireEvent("local_kicked", {room: room});
             });
             this.socket.on('banned', function(room){
-                self.buildClientEvent("banned", room);
+                self.fireEvent("local_banned", {room: room});
             });
             this.socket.on('unbanned', function(room){
-                self.buildClientEvent("unbanned", room);
+                self.fireEvent("local_unbanned", {room: room});
             });
 
             // Room events
-            this.socket.on('r_resource', function(user, resources){
-                self.buildRoomEvent("resource", user, resources);
-            });
             this.socket.on('r_message', function(user, message){
-                self.buildRoomEvent("message", user, message);
+                self.fireEvent("remote_message", {user: user, message: message});
             });
             this.socket.on('r_joined', async function(user){
-                self.buildRoomEvent("joined", user);
+                self.fireEvent("remote_joined", {user: user});
             });
             this.socket.on('r_left', function(user){
-                self.buildRoomEvent("left", user);
+                self.fireEvent("remote_left", {user: user});
             });
             this.socket.on('r_kicked', function(user){
-                self.buildRoomEvent("kicked", user);
+                self.fireEvent("remote_kicked", {user: user});
             });
             this.socket.on('r_banned', function(user){
-                self.buildRoomEvent("banned", user);
+                self.fireEvent("remote_banned", {user: user});
             });
             this.socket.on('r_unbanned', function(user){
-                self.buildRoomEvent("unbanned", user);
+                self.fireEvent("remote_unbanned", {user: user});
             });
         };
     };
